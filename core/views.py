b79fac3e4305c9871630305616b0adb3f99bcd74
jonas <jonas.peixoto.087@gmail.com>
2023-03-30 18:51:11 -0300

    criando athenticação de login

--- core/views.py
+++ core/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -42,3 +44,25 @@ def signup(request):
     else:
         print("to aqui")
         return render(request, 'signup.html')
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
